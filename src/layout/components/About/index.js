58364fb97a60449e272aa5d26c1b91cc7fb8fe52
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-07-02 13:16:06 -0500

    updated bio

--- src/layout/components/About/index.js
+++ src/layout/components/About/index.js
@@ -13,15 +13,15 @@ function About() {
                 <div id="bio-text" className="col-12 col-md-7 col-lg-7">
                     <h2 id="bio-header">Bio</h2>
                     <p>
-                            Levi Schwartzberg is a budding developer who is based in Minneapolis. Levi has a B.S. in Physics from the University of Minnesota, which provides some of the analytical impetus for development.
-                            In addition to working on app and web development, Levi enjoys solving more scientific problems, like exploring Fourier analysis and sound synthesis, as well as raw physics and mathematics, such as solving differential equations computationally with C++ scripts.
-                            He hopes to continue to devlop his skills to build more and more intricate, but also practical applications. 
+                            My name is Levi and I am a full stack developer who is based in Minneapolis. I have a B.S. in Physics from the University of Minnesota, which has provided me not only with certain specialized technical know how, but also with general problem solving and research skills that are useful in web development and coding.
+                            In addition to working on app and web development, I enjoy solving more scientific problems, like exploring Fourier analysis and sound synthesis, as well as raw physics and mathematics, such as solving differential equations computationally with C++ scripts.
+                            I am always looking for new fun and challenging projects where I can expand both my coding skills and my technical or scientific abilities. 
                             <br/>
                             <br/>
-                            In addition to coding, Levi's other career path involves performing and working as a professional musician. 
-                            He has recorded several records with a variety of different bands, and has travelled throughout the Midwest and even all the way to France to perform. 
-                            When not playing music or coding, one would likely find Levi enjoying one of his outdoor pursuits. He is an avid cross country skier, and also enjoys climbing and strength training.
-                            During the summer months, Levi also plays plenty of golf and softball. He also enjoys painting and cooking. 
+                            In addition to coding, my other career path involves performing and working as a professional musician. 
+                            I have recorded several records with a variety of different bands, and have travelled throughout the Midwest and even all the way to France to perform. 
+                            If not playing music or coding, one would likely find me enjoying one of my outdoor pursuits. I am an avid cross country skier, and also enjoy climbing and strength training.
+                            During the summer months, I also play plenty of golf and softball. Additionally, I enjoy painting and cooking.  
                     </p>
                 </div>
             </div>
--- src/layout/components/Portfolio/index.js
+++ src/layout/components/Portfolio/index.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import Project from '../Project';
 import MusicLab from '../../../images/MusicLab.jpg';
-import Kitchen from '../../../images/kitchen.jpg';
+import Tech from '../../../images/techblog.jpg';
 import Hackamole from '../../../images/Hackamole.jpg';
 import Campground from '../../../images/Campground.jpg';
-import Weather from '../../../images/Weather.jpg';
+import Quiz from '../../../images/quiz.jpg';
 import './style.css';
 
 function Portfolio() {
     const musicLabDesc = "Music Lab is a community website where users can create accounts, create music with preset sounds, and then share and comment on other users songs. For this project, I constructed the entire drag and drop functionality (vanilla JS), configured the audio play algorithms, managed the database (MySQL), and assisted with the handlebars layout. I also composed and recorded all the sound clips.";
     const hackaMoleDesc = "Hack-a-Mole is a version of the classic Whack-a-Mole arcade game. The project uses the full MERN stack to control game logic, data flow, and the server. I focused mainly on creating the game logic with React state. I assisted with the database (MongoDB) and component creation. I also composed and recorded the video game music.";
+    const campDesc = "Campground finder is a web application that allows users to search for campgrounds at national parks. A user enters in a city and the application uses and API to find all the nearest campgrounds to that city. The user can then select a specific campsite and view details about it, including the upcoming weather, and google maps directions. My role in the project was to configure the national parks API calls based on the search, sort the data, and then display it. I also built the weather API functionality.";
+    const techDesc = "Tech Blog is an exercise in managing user data in a forum-like setting. Users can create accounts, and then create posts and comment on existing posts. I built this using handlebars and javascript. Data is managed with MySQL."
+    const quizDesc = "This code quiz was created as an exercise in vanilla javascript. I used timers to govern a quiz that randomly chooses a random question from an array and notifies the user when they answer if they are correct. The app keeps track of the score and then can store the users info and their score in local storage upon completion."
 
     return (
         <div className="portfolio-background">
             <div className="portfolio" id="portfolio">
                 <h2>Portfolio</h2>
                 <div className="row">
-                    <Project classes="card col-12" title="Music Lab" image={MusicLab} link="https://polar-lake-37415.herokuapp.com/" alt="Music Lab homepage image" description={musicLabDesc}/>
+                    <Project classes="card col-12" title="Music Lab" image={MusicLab} link="https://polar-lake-37415.herokuapp.com/" alt="Music Lab homepage" description={musicLabDesc}/>
                     <Project classes="card col-12 col-lg-6" title="Hack-a-Mole" image={Hackamole} link="https://enigmatic-sierra-05882.herokuapp.com/" alt="Hack-a-Mole sample gameplay" description={hackaMoleDesc}/>
-                    <Project classes="card col-12 col-lg-6" title="Levi's Kitchen" image={Kitchen} link="https://levischwartzberg.github.io/Levis_Kitchen/" alt="Restaurant homepage image"/>
-                    <Project classes="card col-12 col-lg-6" title="Campground Finder" image={Campground} link="https://lpaschka37.github.io/Project1/" alt="Sample campground info page"/>
-                    <Project classes="card col-12 col-lg-6" title="Weather Dashboard" image={Weather} link="https://levischwartzberg.github.io/Weather_Dashboard/"/>
+                    <Project classes="card col-12 col-lg-6" title="Campground Finder" image={Campground} link="https://lpaschka37.github.io/Project1/" alt="Sample campground info page" description={campDesc}/>
+                    <Project classes="card col-12 col-lg-6" title="Tech Blog" image={Tech} link="https://still-sands-34680.herokuapp.com/" alt="Sample blog post previews" description={techDesc}/>
+                    <Project classes="card col-12 col-lg-6" title="Code Quiz" image={Quiz} link="https://levischwartzberg.github.io/Code_Quiz/" description={quizDesc}/>
                 </div>
             </div>
         </div>
